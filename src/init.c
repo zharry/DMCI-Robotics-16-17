9f8ddaffe530139f38986b9ed6afedc33e3edf4d
Harry Zhang <zh.harry@yahoo.ca>
2016-05-10 20:32:16 -0400

    Testing commit

--- src/init.c
+++ src/init.c
@@ -25,6 +25,7 @@
   * The purpose of this function is solely to set the default pin modes (pinMode()) and port states (digitalWrite()) of limit switches, push buttons, and solenoids. It can also safely configure a UART port (usartOpen()) but cannot set up an LCD (lcdInit()).
   */
 void initializeIO() {
+	Test();
 }
 
 /**
